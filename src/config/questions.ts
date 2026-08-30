e6d580cde6cfec9beb87eddc70f1c0ae59adfe2f
salvatorelj4-jpg <salvatorelj4@gmail.com>
2026-08-30 11:24:42 -0300

    Add whitelistQuestions for user input validation

---
+++ src/config/questions.ts
@@ -0,0 +1,12 @@
+export const whitelistQuestions = [
+  "Informe seu Steam ID64.",
+  "Qual é o seu nome de usuário no Discord?",
+  "Qual é o nome completo do seu personagem?",
+  "Qual é a sua idade?",
+  "Em que ano se passa o Oblivion Stalker e em relação a qual acontecimento da história de S.T.A.L.K.E.R.?",
+  "Explique como seu personagem começa sua jornada na Zona e por que ele ainda não deve possuir conhecimento avançado.",
+  "Você descobre pelo Discord a localização de um esconderijo. Seu personagem pode usar essa informação? Explique.",
+  "Seu personagem morre sem sofrer Permadeath. Explique o que acontece segundo a NLR.",
+  "Durante uma situação hostil, seu personagem está sozinho contra vários homens fortemente armados. Como você reagiria e por quê?",
+  "Conte a história do seu personagem antes de chegar à Zona, o motivo que o levou até ela em 2012 e quais objetivos pretende perseguir no início de sua jornada."
+];
